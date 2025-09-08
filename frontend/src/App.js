3326ca1a184ab257a17cba5e0ca773e3adf0ec81
Kuldeep Singh <singh8kuldeep1802@gmail.com>
2025-09-08 08:25:40 +0530

    Cleanup: Remove unnecessary root package.json and package-lock.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Import components
+import Home from './components/Home/Home';
+import UserProfile from './components/UserProfile/UserProfile';
+import RoadTripsList from './components/RoadTripsList/RoadTripsList';
+import RoadTripDetails from './components/RoadTripDetails/RoadTripDetails';
 
 function App() {
   return (
-    <div>
-      <h1>Road Trip App</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<UserProfile />} />
+        <Route path="/trips" element={<RoadTripsList />} />
+        <Route path="/trips/:id" element={<RoadTripDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
-
